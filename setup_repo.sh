c1286ed8e60fe57138631e9570939be2ba28d097
Shankar Kulumani <shanks.k@gmail.com>
2017-09-13 15:22:32 -0400

    Add setup repo script

---
+++ setup_repo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# add the correct remote repositories
+git_repo="git@github.com:skulumani/research_summary.git"
+bb_repo="git@bitbucket.org:shankarkulumani/research-summary.git"
+
+printf "Setting the origin remote to point to Github and Bitbucket:\n\n"
+printf "Github: $git_repo\n"
+printf "Bitbucket: $bb_repo\n\n"
+
+
+git remote set-url origin --push --add $bb_repo
+git remote set-url origin --push --add $git_repo
+
+printf "Finished! The new remotes are listed.\n"
+printf "\n"
+
+git remote -v
