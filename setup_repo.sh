015a59baa220f5119171db71d06a788cfc4f8b5d
Shankar Kulumani <shanks.k@gmail.com>
2017-09-13 17:27:05 -0400

    Fix setup repo

--- setup_repo.sh
+++ setup_repo.sh
@@ -8,7 +8,6 @@ printf "Setting the origin remote to point to Github and Bitbucket:\n\n"
 printf "Github: $git_repo\n"
 printf "Bitbucket: $bb_repo\n\n"
 
-
 git remote set-url origin --push --add $bb_repo
 git remote set-url origin --push --add $git_repo
 
